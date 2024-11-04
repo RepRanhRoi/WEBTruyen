7659ceb309dceaa96bd80c9aae76f2adf498e26f
wish-ope <lead4380@gmail.com>
2024-11-04 13:09:01 +0700

    mmm

--- obj/Debug/net8.0/WebTruyen.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebTruyen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTruyen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d1f06c97afd2f3df64f9458f4386f86aec8cc70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd8c2964343840ba18e06e0ea9a1cae55a0605c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTruyen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTruyen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
